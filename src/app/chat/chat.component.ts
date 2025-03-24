e44f0915b0424386d9b6fad0fb2f895a82bfc705
Alex-School0-0x <alth2@elev.tec.dk>
2025-03-24 14:08:19 +0100

    Chat 001

--- src/app/chat/chat.component.ts
+++ src/app/chat/chat.component.ts
@@ -6,6 +6,43 @@ import { Component } from '@angular/core';
   templateUrl: './chat.component.html',
   styleUrl: './chat.component.css'
 })
-export class ChatComponent {
 
+export class ChatComponent {
+  msg : Message[] = [];
+  test : TestM[] = [
+    new TestM(1,"ss"),
+    new TestM(2,"ssss")
+  ]
+
+  makeMessage(newMsg:string){
+    this.msg.push(new Message(newMsg))
+  }
+}
+
+
+
+
+
+class Message {
+  message : string;
+  userName : string = "";
+  timeStamp : undefined;
+
+  constructor(message : string) {
+    this.message = message;
+  }
+}
+
+
+
+
+
+class TestM {
+  id : number;
+  text : string;
+
+  constructor(id : number, text : string) {
+    this.id = id;
+    this.text = text;
+  }
 }
